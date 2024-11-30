2dfa8823b7bbc944563e78cf4dd0a832eb610cde
rfs <remo@mountain-yard.ch>
2024-11-30 20:07:45 +0100

    refactor: remove obsolete class

--- 2023/day_1/CalibrationValueExtractor.ts
+++ 2023/day_1/CalibrationValueExtractor.ts
@@ -23,26 +23,3 @@ export const convertWithRulesForPart1: (input: string) => number = (
     10,
   );
 };
-
-export class CalibrationValueExtractor {
-  static parseNumberForInputOfPart1(calibrationValueAsString: string): number {
-    const extractedValues: string[] = [];
-    iterateString(
-      calibrationValueAsString,
-      isDigit,
-      (match) => extractedValues.push(match),
-      true,
-    );
-    iterateStringReverse(
-      calibrationValueAsString,
-      isDigit,
-      (match) => extractedValues.push(match),
-      true,
-    );
-
-    return parseInt(
-      extractedValues.reduce((acc, current) => acc + current, ""),
-      10,
-    );
-  }
-}
