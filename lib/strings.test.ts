d80cc5d44e1bd4d6f8154c8174eaa0daef28459d
rfs <remo@mountain-yard.ch>
2024-11-30 20:44:39 +0100

    refactor: implement tests for string iteration

--- lib/strings.test.ts
+++ lib/strings.test.ts
@@ -1,6 +1,6 @@
 import { describe, test } from "jsr:@std/testing/bdd";
 import { assertEquals } from "jsr:@std/assert";
-import { isDigit } from "@strings";
+import { isDigit, iterateString, iterateStringReverse } from "@strings";
 
 describe("The string utilities", () => {
   describe("when checking for digits for all ASCII chars", () => {
@@ -20,7 +20,7 @@ describe("The string utilities", () => {
         const result = isDigit(char);
 
         // then
-        assertEquals(true, result, `Failed for digit: ${char}`);
+        assertEquals(result, true, `Failed for digit: ${char}`);
       }
     });
 
@@ -33,8 +33,148 @@ describe("The string utilities", () => {
         const result = isDigit(char);
 
         // then
-        assertEquals(false, result, `Failed for non-digit: ${char}`);
+        assertEquals(result, false, `Failed for non-digit: ${char}`);
       }
+    });
+  });
+
+  describe("when iterating over a string", () => {
+    type IterationMatch = {
+      index: number;
+      originalValue: string;
+      mappedValue: string;
+    };
+    test("should identify matches by default", () => {
+      // given
+      const someString = "abca";
+      const accumulator: IterationMatch[] = [];
+
+      // when
+      iterateString(
+        someString,
+        (value) => value === "a" || value === "c",
+        (match, i) => (accumulator.push({
+          index: i,
+          originalValue: match,
+          mappedValue: match + "-mapped",
+        } as IterationMatch)),
+      );
+
+      // then
+      assertEquals(accumulator, [
+        {
+          index: 0,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+        {
+          index: 2,
+          originalValue: "c",
+          mappedValue: "c-mapped",
+        },
+        {
+          index: 3,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+      ]);
+    });
+
+    test("should identify first matche if we break on match", () => {
+      // given
+      const someString = "abca";
+      const accumulator: IterationMatch[] = [];
+
+      // when
+      iterateString(
+        someString,
+        (value) => value === "a" || value === "c",
+        (match, i) => (accumulator.push({
+          index: i,
+          originalValue: match,
+          mappedValue: match + "-mapped",
+        } as IterationMatch)),
+        true,
+      );
+
+      // then
+      assertEquals(accumulator, [
+        {
+          index: 0,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+      ]);
+    });
+  });
+
+  describe("when iterating reverse over a string", () => {
+    type IterationMatch = {
+      index: number;
+      originalValue: string;
+      mappedValue: string;
+    };
+    test("should identify matches by default", () => {
+      // given
+      const someString = "abca";
+      const accumulator: IterationMatch[] = [];
+
+      // when
+      iterateStringReverse(
+        someString,
+        (value) => value === "a" || value === "c",
+        (match, i) => (accumulator.push({
+          index: i,
+          originalValue: match,
+          mappedValue: match + "-mapped",
+        } as IterationMatch)),
+      );
+
+      // then
+      assertEquals(accumulator, [
+        {
+          index: 3,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+        {
+          index: 2,
+          originalValue: "c",
+          mappedValue: "c-mapped",
+        },
+        {
+          index: 0,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+      ]);
+    });
+
+    test("should identify first matche if we break on match", () => {
+      // given
+      const someString = "abca";
+      const accumulator: IterationMatch[] = [];
+
+      // when
+      iterateStringReverse(
+        someString,
+        (value) => value === "a" || value === "c",
+        (match, i) => (accumulator.push({
+          index: i,
+          originalValue: match,
+          mappedValue: match + "-mapped",
+        } as IterationMatch)),
+        true,
+      );
+
+      // then
+      assertEquals(accumulator, [
+        {
+          index: 3,
+          originalValue: "a",
+          mappedValue: "a-mapped",
+        },
+      ]);
     });
   });
 });
