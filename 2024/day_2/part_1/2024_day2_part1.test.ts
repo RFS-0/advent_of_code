e8564d0335be90e152a2ed5e09258b5a85ebe1e0
rfs <remo@mountain-yard.ch>
2024-12-01 15:51:30 +0100

    feat: add setup for part 1 and 2 of day 2 in 2024

---
+++ 2024/day_2/part_1/2024_day2_part1.test.ts
@@ -0,0 +1,25 @@
+import { describe, test } from "jsr:@std/testing/bdd";
+import { InputParser } from "@input";
+
+import { assertEquals } from "jsr:@std/assert";
+import { parse } from "../day2-utils.ts";
+
+describe("Advent of Code 2024, Day 2, Part 1", () => {
+  test("should be able to solve example", () => {
+    const input = `tbd`;
+
+    const parsed = new InputParser(input)
+      .printInput()
+      .parseLines(parse)
+      .printParsed()
+      .getParsed();
+
+    // process parsed
+
+    // calc result from processed
+
+    const result = undefined;
+
+    assertEquals(result, "tbd");
+  });
+});
---
+++ 2024/day_2/part_1/2024_day2_part1.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S deno run --allow-net --allow-env
+
+// execute from root with: deno run --allow-net 2024/day_2/part_1/2024_day2_part1.ts <session_cookie>
+
+import { downloadInput, InputParser, readSessionToken } from "@input";
+import { parse } from "../day2-utils.ts";
+import { uploadSolution } from "@output";
+
+const sessionCookie = readSessionToken();
+const input = await downloadInput(
+  "https://adventofcode.com/2024/day/2/input",
+  sessionCookie,
+);
+
+const parsed = new InputParser(input)
+  .printInput()
+  .parseLines(parse)
+  .printParsed()
+  .getParsed();
+
+// process parsed
+
+// calc result from processed
+
+const result = "tbd";
+
+console.log("Submitting the result: ", result);
+
+await uploadSolution(
+  "https://adventofcode.com/2024/day/2/answer",
+  "1",
+  result,
+  sessionCookie,
+);
